f909ca45d54bd9cd83c99d6310006624108dded8
Anand Kumar <as1273534@gmail.com>
2025-12-24 13:21:00 +0530

    Create weatherapi.py

---
+++ weatherapi.py
@@ -0,0 +1,17 @@
+import requests
+
+#weather api
+def get_weather(getCity):
+    city = getCity
+    weather_key = os.environ.get('WEATHER_API')
+    url = f"http://api.weatherapi.com/v1/current.json?key={weather_key}&q={city}"
+    res = requests.get(url)
+    
+    if res.status_code == 200 :
+        wdict = res.json()
+        
+        loc = wdict["location"]
+        cur  = wdict["current"]
+        return loc , cur
+    else : 
+        return "City not found"
